d6f39a0e1bd10af4f47b0c5b0147fffcdbed75cb
Lucifer0885 <georgeskotidismain@gmail.com`>
2025-06-09 01:50:43 +0300

    update: socket connection URL to use environment variable for production

--- src/api/sockets.ts
+++ src/api/sockets.ts
@@ -1,5 +1,7 @@
 import { io } from 'socket.io-client';
 
-const SOCKET_URL: string = 'http://localhost:3033';
+const SOCKET_URL = import.meta.env.PROD
+        ? "https://sockets.efood.pagonoudis.gr"
+        : "http://localhost:3033";
 
 export const socket = io(SOCKET_URL);
